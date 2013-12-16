dfefb59f7521bbae750aadb50085c40c62da95f0
ctcarrier <ctcarrier@gmail.com>
2013-12-16 13:50:06 -0700

    Got basic tests working

--- src/test/scala/com/blrest/ImageDirectoryEndpointSpec.scala
+++ src/test/scala/com/blrest/ImageDirectoryEndpointSpec.scala
@@ -4,18 +4,24 @@ import org.specs2.mutable.Specification
 import spray.testkit.Specs2RouteTest
 import com.blrest.endpoint.ImageDirectoryEndpoint
 import com.blrest.dao.ImageDirectoryDao
-import org.scalamock.specs2.MockFactory
 import com.blrest.model.ImageMeta
 import scala.util.Random
+import spray.http.StatusCodes._
+import spray.http.ContentTypes.`application/json`
+
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+import scala.concurrent.Future
 
 /**
  * Created by ccarrier for bl-rest.
  * at 10:07 PM on 12/15/13
  */
-class ImageDirectoryEndpointSpec extends Specification with Specs2RouteTest with ImageDirectoryEndpoint with MockFactory {
+class ImageDirectoryEndpointSpec extends Specification with Specs2RouteTest with ImageDirectoryEndpoint {
 
   def actorRefFactory = system
-  
+  implicit val formats = DefaultFormats
+
   val dummyImageMeta = ImageMeta(
     Random.nextLong(),
     "url",
@@ -35,14 +41,39 @@ class ImageDirectoryEndpointSpec extends Specification with Specs2RouteTest with
 
   "The service" should {
 
-    "return a greeting for GET requests to the root path" in {
-      Get() ~> imageDirectoryRoute ~> check {
-        responseAs[String] must contain("Say hello")
+    "return an ImageMeta for direct GET requests" in {
+      Get("/images/%s".format(dummyImageMeta.flickr_id)) ~> imageDirectoryRoute ~> check {
+        responseAs[ImageMeta] == dummyImageMeta
+        contentType === `application/json`
+        status === OK
+      }
+    }
+    "return a 404 for a non-existant ImageMeta for GET requests" in {
+      Get("/images/%s".format(000)) ~> imageDirectoryRoute ~> check {
+        status === NotFound
+      }
+    }
+    "return a random ImageMeta for GET requests" in {
+      Get("/images/random") ~> imageDirectoryRoute ~> check {
+        responseAs[ImageMeta] == dummyImageMeta
+        contentType === `application/json`
+        status === OK
       }
     }
   }
-  val mockDao: ImageDirectoryDao = mock[ImageDirectoryDao]
+  val imageDirectoryDao = new ImageDirectoryDao {
+    def getImageMetaData(key: Long): Future[Option[ImageMeta]] = {
+      if (key == dummyImageMeta.flickr_id) {
+        return Future.successful(Some(dummyImageMeta))
+      }
+      else {
+        return Future.successful(None)
+      }
+    }
 
-  mockDao expects 'getImageMetaData withArgs(123l)
+    def getRandomImageMetaData: Future[Option[ImageMeta]] = {
+      return Future.successful(Some(dummyImageMeta))
+    }
+  }
 
 }
