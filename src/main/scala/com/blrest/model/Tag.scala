eff0888b9a28178a28312eb2309e094640950faa
ctcarrier <ctcarrier@gmail.com>
2013-12-24 00:22:55 -0700

    Refactoring tag structure

--- src/main/scala/com/blrest/model/Tag.scala
+++ src/main/scala/com/blrest/model/Tag.scala
@@ -4,7 +4,11 @@ package com.blrest.model
  * Created by ccarrier for bl-rest.
  * at 1:44 PM on 12/20/13
  */
-case class Predicate(displayPattern: String, key: String)
-case class Tag(name: String, predicate: Predicate)
+case class Predicate(key: String)
+case object Predicate {
+  def IS = Predicate("IS")
+  def HAS = Predicate("HAS")
+}
+case class Tag(name: String, displayPattern: String, predicate: Predicate)
 
 case class TagResponse(tag: Tag, response: Boolean)
